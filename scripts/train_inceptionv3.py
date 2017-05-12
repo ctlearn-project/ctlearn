c52ccafd2640508d9db628863681f464832371aa
Ari Brill <aryeh.brill@columbia.edu>
2017-05-12 11:56:34 -0400

    Update train_inceptionv3.py to run many models

--- scripts/train_inceptionv3.py
+++ scripts/train_inceptionv3.py
@@ -3,9 +3,10 @@ import os
 from shutil import copyfile
 import sys
 
-import numpy
+import numpy as np
 
 from keras.models import Model
+from keras.optimizers import SGD, RMSprop
 from keras.layers.core import Dense, Flatten
 from keras.applications.inception_v3 import InceptionV3
 from keras.preprocessing.image import ImageDataGenerator
@@ -15,8 +16,8 @@ from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping, ReduceLRO
 image_x_dim= 120
 image_y_dim= 120
 
-training_samples = 10
-validation_samples = 10
+training_samples = 40000
+validation_samples = 10000
 
 # parse command line arguments
 
@@ -36,26 +37,10 @@ args = parser.parse_args()
 train_data_path = os.path.abspath(args.train_data_dir)
 val_data_path = os.path.abspath(args.val_data_dir)
 
-# Create the InceptionV3 model
-initial_model = InceptionV3(
-        include_top=False, 
-        weights=None, 
-        input_shape=(3, image_x_dim*2, image_y_dim*2), 
-        pooling=None)
-last = initial_model.output
-
-x = Flatten()(last)
-prediction = Dense(1, activation='sigmoid')(x)
-model = Model(initial_model.input, prediction)
-model.compile(
-        optimizer='sgd',
-        loss='binary_crossentropy',
-        metrics=['binary_accuracy'])
-
-# Load saved weights
-if args.weights is not None:
-    model_weights_path = os.path.abspath(args.weights)
-    model.load_weights(model_weights_path)
+## Load saved weights
+#if args.weights is not None:
+#    model_weights_path = os.path.abspath(args.weights)
+#    model.load_weights(model_weights_path)
 
 # Create required directories
 ############################
@@ -104,21 +89,47 @@ validation_generator = validation_preprocess.flow_from_directory(
 #train model
 ############
 
-logger = CSVLogger(os.path.join(run_dir, args.run_name + '.log'),separator=',', append=True)
-checkpoint = ModelCheckpoint(os.path.join(checkpoint_dir,args.run_name + '-{epoch:04d}-{val_loss:.5f}.h5'), monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=True, mode='auto')
+checkpoint = ModelCheckpoint(os.path.join(checkpoint_dir,
+    args.run_name + '-{epoch:04d}-{val_loss:.5f}.h5'),
+    monitor='val_loss',
+    verbose=0,
+    save_best_only=False,
+    save_weights_only=True,
+    mode='auto')
 earlystoploss = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=10, verbose=0, mode='auto')
 earlystopacc = EarlyStopping(monitor='val_binary_accuracy', min_delta=0.001, patience=5, verbose=0, mode='auto')
 reducelr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, verbose=0, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
-
-#class weights
-#class_weight = {0:17,1:83}
-
-history = model.fit_generator(
-        generator=training_generator,
-        steps_per_epoch=int(training_samples/args.batch_size),
-        epochs=args.epochs,
-        callbacks=[logger, checkpoint],
-        validation_data=validation_generator,
-        validation_steps=int(validation_samples/args.batch_size),
-        class_weight='auto')
-
+lrs = 2*np.random.random(25) - 4
+lrs = 10**lrs
+accs = []
+for lr in lrs:
+    # Create the InceptionV3 model
+    initial_model = InceptionV3(
+            include_top=False, 
+            weights=None, 
+            input_shape=(3, image_x_dim*2, image_y_dim*2), 
+            pooling=None)
+    last = initial_model.output
+    x = Flatten()(last)
+    prediction = Dense(1, activation='sigmoid')(x)
+    model = Model(initial_model.input, prediction)
+    
+    model.compile(
+            optimizer=SGD(lr=lr),
+            loss='binary_crossentropy',
+            metrics=['binary_accuracy'])
+    
+    logger = CSVLogger(os.path.join(run_dir, args.run_name + str(lr) + '.log'),
+            separator=',', append=True)
+    history = model.fit_generator(
+            generator=training_generator,
+            steps_per_epoch=int(training_samples/args.batch_size),
+            epochs=args.epochs,
+            callbacks=[logger],
+            validation_data=validation_generator,
+            validation_steps=int(validation_samples/args.batch_size),
+            class_weight='auto')
+    accs.append(history.history["val_binary_accuracy"][-1])
+sorted_lrs = [(lr, acc) for (acc, lr) in sorted(zip(accs, lrs), reverse=True)]
+for sorted_lr in sorted_lrs:
+    print(sorted_lr)
