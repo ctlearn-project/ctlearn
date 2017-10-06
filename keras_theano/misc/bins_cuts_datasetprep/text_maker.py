d7dd088cc7c617e30ba7ad5662ff99136f43ba02
Bryan Kim <bsk2133@columbia.edu>
2017-10-06 14:35:25 -0400

    Updated file structure

--- keras_theano/misc/bins_cuts_datasetprep/text_maker.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-text_file= open('gamma_proton.txt','w')
-for x in range (0,80):
-    x_int=str(x)
-    text_file.write('gamma-diffuse_'+x_int+'.txt\n')
-    text_file.write('proton_'+x_int+'.txt\n')
-text_file.close()
