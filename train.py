baba89235ee668f6126421759c6301615dcb3da0
Ari Brill <aryeh.brill@gmail.com>
2017-11-13 20:14:28 -0500

    Move HDF5 data loading functions to separate file
    All functionality relating to the specific data format has been moved to
    its own file. This way, the same train script can be used regardless of
    changes to the data format.

--- train.py
+++ train.py
@@ -9,85 +9,6 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 slim = tf.contrib.slim
-import numpy as np
-import tables
-
-def load_HDF5_data(filename, index, metadata, mode='TRAIN'):
-
-    # Read the data at the given table and index from the file
-    f = tables.open_file(filename, mode='r')
-    if mode == 'TRAIN':
-        table = f.root.E0.Events_Training
-    elif mode == 'VALID':
-        table = f.root.E0.Events_Validation
-    else:
-        raise ValueError("Mode must be 'TRAIN' or 'VALID'")
-    record = table.read(index, index + 1)
-    
-    telescope_ids = metadata['telescope_ids']
-    image_indices = record['tel_map'][0]
-    telescope_images = []
-    for telescope_id, image_index in zip(telescope_ids, image_indices):
-        if image_index == -1:
-            # Telescope did not trigger. Its outputs will be dropped
-            # out, so input is arbitrary. Use an empty array for
-            # efficiency.
-            telescope_images.append(np.empty(metadata['image_shape']))
-        else:
-            telescope_table = f.root.E0._f_get_child(telescope_id)
-            telescope_images.append(telescope_table[image_index])
-    telescope_images = np.stack(telescope_images).astype(np.float32)
-    
-    # Get binary values indicating whether each telescope triggered
-    telescope_triggers = np.array([0 if i < 0 else 1 for i in image_indices],
-            dtype=np.int8)
-    
-    # Get classification label by converting CORSIKA particle code
-    gamma_hadron_label = record['gamma_hadron_label'][0]
-    if gamma_hadron_label == 0: # gamma ray
-        gamma_hadron_label = 1
-    elif gamma_hadron_label == 101: # proton
-        gamma_hadron_label = 0
-    
-    f.close()
-    
-    return [telescope_images, telescope_triggers, gamma_hadron_label]
-
-def load_HDF5_auxiliary_data(filename):
-    
-    f = tables.open_file(filename, mode='r')
-    telescope_positions = []
-    for row in f.root.Tel_Table.iterrows():
-        telescope_positions.append(row["tel_x"])
-        telescope_positions.append(row["tel_y"])
-    f.close()
-    auxiliary_data = {
-        'telescope_positions': np.array(telescope_positions, dtype=np.float32)
-        }
-    return auxiliary_data
-
-def load_HDF5_metadata(filename):
-   
-    f = tables.open_file(filename, mode='r')
-    num_training_events = f.root.E0.Events_Training.shape[0]
-    num_validation_events = f.root.E0.Events_Validation.shape[0]
-    # List of telescope IDs ordered by mapping index
-    telescope_ids = ["T" + str(row["tel_id"]) for row 
-            in f.root.Tel_Table.iterrows()]
-    num_telescopes = f.root.Tel_Table.shape[0]
-    # All telescope images have the same shape
-    image_shape = f.root.E0._f_get_child(telescope_ids[0]).shape[1:]
-    f.close()
-    metadata = {
-            'num_training_events': num_training_events,
-            'num_validation_events': num_validation_events,
-            'telescope_ids': telescope_ids,
-            'num_telescopes': num_telescopes,
-            'image_shape': image_shape,
-            'num_auxiliary_inputs': 2,
-            'num_gamma_hadron_classes': 2
-            }
-    return metadata
 
 # Parse configuration file
 config = configparser.ConfigParser()
@@ -131,9 +52,9 @@ shutil.copy(config_filename, os.path.join(model_dir, config_log_filename))
 
 # Define data loading functions
 if use_hdf5_format:
-    load_data = load_HDF5_data
-    load_auxiliary_data = load_HDF5_auxiliary_data
-    load_metadata = load_HDF5_metadata
+    from load_HDF5_data import load_HDF5_data as load_data
+    from load_HDF5_data import load_HDF5_auxiliary_data as load_auxiliary_data
+    from load_HDF5_data import load_HDF5_metadata as load_metadata
 else:
     sys.exit("Error: No data format specified.")
 
