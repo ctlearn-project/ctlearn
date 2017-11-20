9836d2e66f76fedafcf3ef5166a3a95da59097b6
Bryan Kim <bsk2133@columbia.edu>
2017-11-20 15:06:08 -0500

    Returned warning level to 2.

--- ctalearn/scripts/train.py
+++ ctalearn/scripts/train.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 # Disable info and warning messages (not error messages)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
 slim = tf.contrib.slim
