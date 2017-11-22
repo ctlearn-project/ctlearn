2ad77e1f346050cdc1da0f3eef318f4dba92b386
Bryan Kim <bsk2133@columbia.edu>
2017-11-21 23:03:14 -0500

    Added new function for reading data for cnn_rnn

--- ctalearn/data.py
+++ ctalearn/data.py
@@ -2,6 +2,52 @@ import tables
 import numpy as np
 
 
+def load_HDF5_data_by_tel(filename, index, metadata, mode='TRAIN'):
+
+    # Read the data at the given table and index from the file
+    f = tables.open_file(filename, mode='r')
+    if mode == 'TRAIN':
+        table = f.root.E0.Events_Training
+    elif mode == 'VALID':
+        table = f.root.E0.Events_Validation
+    else:
+        raise ValueError("Mode must be 'TRAIN' or 'VALID'")
+    record = table.read(index, index + 1)
+
+    tel_data_table = f.root.Tel_Table
+
+    telescope_ids = metadata['telescope_ids']
+    image_indices = record['tel_map'][0]
+    telescope_images = []
+    telescope_positions = []
+    for telescope_id, image_index in zip(telescope_ids, image_indices):
+       if image_index != -1:
+            telescope_table = f.root.E0._f_get_child(telescope_id)
+            telescope_images.append(telescope_table[image_index])
+            telescope_position = [[x['tel_x'],x['tel_y'],x['tel_z']] for x in tel_data_table.iterrows() if x['tel_id'] == int(telescope_id[1:])][0]
+            telescope_positions.append(telescope_position)
+
+    while len(telescope_images) < len(telescope_ids):
+        telescope_images.append(np.zeros(metadata['image_shape']))
+        telescope_positions.append([0,0,0])
+ 
+    telescope_positions = np.stack(telescope_positions).astype(np.float32)
+    telescope_images = np.stack(telescope_images).astype(np.float32)
+
+    # Get binary values indicating whether each telescope triggered
+    telescope_triggers = np.array([0 if i < 0 else 1 for i in image_indices].sort(reverse=True),dtype=np.int8)
+    
+    # Get classification label by converting CORSIKA particle code
+    gamma_hadron_label = record['gamma_hadron_label'][0]
+    if gamma_hadron_label == 0: # gamma ray
+        gamma_hadron_label = 1
+    elif gamma_hadron_label == 101: # proton
+        gamma_hadron_label = 0
+    
+    f.close()
+    
+    return [telescope_images, telescope_triggers, telescope_positions, gamma_hadron_label]
+
 def load_HDF5_data(filename, index, metadata, mode='TRAIN'):
 
     # Read the data at the given table and index from the file
