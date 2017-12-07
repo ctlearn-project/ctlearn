0f1aec14d753341d34acabe9a5548c02b0b3295e
Bryan Kim <bsk2133@columbia.edu>
2017-12-07 17:42:31 -0500

    Changed to use cnn_block instead of cnn_base (with trigger dropout toggled off).

---
+++ ctalearn/models/single_tel.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+
+
+def single_tel_model(features, labels, params, is_training):
+    
+    # Reshape and cast inputs into proper dimensions and types
+    image_width, image_length, image_depth = params['image_shape']
+    num_gamma_hadron_classes = params['num_gamma_hadron_classes']
+    
+    telescope_data = features['telescope_data']
+    telescope_data = tf.reshape(telescope_data, [-1, 
+        image_width, image_length, image_depth])
+    telescope_data = tf.cast(telescope_data, tf.float32)
+ 
+    # Reshape labels to vector as expected by tf.one_hot
+    gamma_hadron_labels = labels['gamma_hadron_labels']
+    gamma_hadron_labels = tf.reshape(gamma_hadron_labels, [-1])
+    gamma_hadron_labels = tf.cast(gamma_hadron_labels, tf.int32)
+
+    # Choose the CNN base
+    if params['cnn_block'] == 'alexnet':
+        from ctalearn.models.alexnet import alexnet_block as cnn_block
+    elif params['cnn_block'] == 'mobilenet':
+        from ctalearn.models.mobilenet import mobilenet_block as cnn_block
+    elif params['cnn_block'] == 'resnet':
+        from ctalearn.models.resnet import resnet_block as cnn_block
+    else:
+        sys.exit("Error: No valid CNN block specified.")
+
+    output = cnn_block(telescope_data,None,
+                params=params,
+                is_training=is_training)
+
+    #flatten output of embedding CNN to (batch_size, _)
+    shape = output.get_shape().as_list()
+    dim = np.prod(shape[1:])
+    output_flattened = tf.reshape(output,[-1,dim])
+
+    fc1 = tf.layers.dense(inputs=output_flattened, units=1024, activation=tf.nn.relu)
+    
+    logits = tf.layers.dense(inputs=last_output,units=num_gamma_hadron_classes)
+
+    onehot_labels = tf.one_hot(
+            indices=gamma_hadron_labels,
+            depth=num_gamma_hadron_classes)
+
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, 
+            logits=logits)
+
+    return loss, logits
