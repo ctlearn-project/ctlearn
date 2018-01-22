da49e41601ee367acb996d5ccb7f3b96d1c4969f
Bryan Kim <bsk2133@columbia.edu>
2018-01-21 19:21:37 -0500

    Cleaned up/condensed code for most data loading functions

--- ctalearn/data.py
+++ ctalearn/data.py
@@ -8,14 +8,15 @@ IMAGE_SHAPES = {
         'MSTS': (120,120,1)
         }
 
+MAPPING_TABLES = {
+        'MSTS': __generate_table_MSTS()
+        }
+
 def __generate_table_MSTS():
     """
-    Function returning MSTS mapping table
+    Function returning MSTS mapping table (used to index into the trace when converting from trace to image).
     """
     
-    LENGTH = 120
-    WIDTH = 120
-
     ROWS = 15
     MODULE_DIM = 8
     MODULES_PER_ROW = [
@@ -43,11 +44,11 @@ def __generate_table_MSTS():
                               for j in range(ROWS)
                               for i in range(MODULES_PER_ROW[j])]
 
+    table = np.zeros(shape=(IMAGE_SHAPES['MSTS'][0],IMAGE_SHAPES['MSTS'][1]),dtype=int)   
     # Fill appropriate positions with indices
     # NOTE: we append a 0 entry to the (11328,) trace array to allow us to use fancy indexing to fill
     # the empty areas of the (120,120) image. Accordingly, all indices in the mapping table are increased by 1
     # (j starts at 1 rather than 0)
-    table = np.zeros(shape=(LENGTH,WIDTH),dtype=int)  
     j = 1
     for (x_0,y_0) in MODULE_START_POSITIONS:
         for i in range(MODULE_DIM * MODULE_DIM):
@@ -58,13 +59,7 @@ def __generate_table_MSTS():
 
     return table
 
-
-MAPPING_TABLES = {
-        'MSTS': __generate_table_MSTS()
-        }
-
-# Functions for locking when opening and closing the same file in multiple threads
-
+# Multithread-safe PyTables open and close file fns
 lock = threading.Lock()
 
 def synchronized_open_file(*args, **kwargs):
@@ -76,6 +71,7 @@ def synchronized_close_file(self, *args, **kwargs):
         return self.close(*args, **kwargs)
 
 # Generator function used to produce a dataset of elements (HDF5_filename,index)
+# from a list of files and a list of lists of indices per file (constructed by applying cuts)
 def HDF5_gen_fn(file_list,indices_by_file):
     for i,filename in enumerate(file_list):
         for j in indices_by_file[i]:
@@ -84,28 +80,27 @@ def HDF5_gen_fn(file_list,indices_by_file):
 # Data loading function for event-wise (array-level) HDF5 data loading
 def load_HDF5_data(filename, index, auxiliary_data, metadata,sort_telescopes_by_trigger=False):
 
-    # Read the data at the given table and index from the file
+    # Read the event record for the given filename and index
     f = synchronized_open_file(filename.decode('utf-8'), mode='r')
     record = f.root.Event_Info[index]
     
     # Get classification label by converting CORSIKA particle code
-    particle_id = record['particle_id']
-    if particle_id == 0: # gamma ray
+    if record['particle_id'] == 0: # gamma ray
         gamma_hadron_label = 1
-    elif particle_id == 101: # proton
+    elif record['particle_id']  == 101: # proton
         gamma_hadron_label = 0
-   
+  
+    # Collect image indices for each telescope type in the data
     telescope_types = metadata['telescope_types']
     image_indices = {tel_type:record[tel_type+"_indices"] for tel_type in telescope_types}
-    # list of telescope images
+   
+    # Collect images and binary trigger values
     telescope_images = []
-    # list of binary telescope triggers
     telescope_triggers = []
     for tel_type in sorted(image_indices):
+        # Only save MSTS (other telescope types do not have valid MAPPING_TABLES yet)
         if tel_type in MAPPING_TABLES:
-            indices = image_indices[tel_type]
-            image_shape = metadata['image_shapes'][tel_type]
-            for i in indices:
+            for i in image_indices[tel_type]:
                 if i == 0:
                     # Telescope did not trigger. Its outputs will be dropped
                     # out, so input is arbitrary. Use an empty array for
@@ -113,13 +108,13 @@ def load_HDF5_data(filename, index, auxiliary_data, metadata,sort_telescopes_by_
                     telescope_images.append(np.empty(metadata['image_shapes']['MSTS']))
                     telescope_triggers.append(0)
                 else:
-                    telescope_image = load_HDF5_image(f,'MSTS',metadata,i)
+                    telescope_image = load_HDF5_image(f,'MSTS',i)
                     telescope_images.append(telescope_image)
                     telescope_triggers.append(1)
     
     synchronized_close_file(f)
 
-    # telescope positions 
+    # Collect flattened telescope positions from auxiliary data
     telescope_positions = auxiliary_data
 
     if sort_telescopes_by_trigger:
@@ -137,7 +132,7 @@ def load_HDF5_data(filename, index, auxiliary_data, metadata,sort_telescopes_by_
         telescope_positions = [i for sublist in telescope_positions for i in
                 sublist]
  
-    # Convert to numpy arrays
+    # Convert to numpy arrays with correct types
     telescope_images = np.stack(telescope_images).astype(np.float32)
     telescope_triggers = np.array(telescope_triggers, dtype=np.int8)
     telescope_positions = np.array(telescope_positions, dtype=np.float32)
@@ -145,42 +140,44 @@ def load_HDF5_data(filename, index, auxiliary_data, metadata,sort_telescopes_by_
     return [telescope_images, telescope_triggers, telescope_positions,
             gamma_hadron_label]
 
-# Data loading function for single tel HDF5 data loading
+# Data loading function for single tel HDF5 data
+# NOTE: Hardcoded to work only for MSTS images
 def load_HDF5_data_single_tel(filename, index, metadata):
 
-    # Read the data at the given table and index from the file
+    # Load image table record from specified file and image table index
     f = synchronized_open_file(filename.decode('utf-8'), mode='r')
+    telescope_image = load_HDF5_image(f,'MSTS',index)
 
-    telescope_image = load_HDF5_image(f,'MSTS',metadata,index)
-
+    # Get corresponding event record using event_index column
     event_index = f.root._f_get_child('MSTS')[index]['event_index']
     event_record = f.root.Event_Info[event_index]
 
     # Get classification label by converting CORSIKA particle code
-    particle_id = event_record['particle_id']
-    if particle_id == 0: # gamma ray
+    if event_record['particle_id'] == 0: # gamma ray
         gamma_hadron_label = 1
-    elif particle_id == 101: # proton
+    elif event_record['particle_id'] == 101: # proton
         gamma_hadron_label = 0
       
     synchronized_close_file(f)
 
     return [telescope_image, gamma_hadron_label]
 
-def load_HDF5_auxiliary_data(file_list):
-  
+# Return dict of auxiliary data values (currently only contains telescope position coordinates).
+# Contains all MSTS telescope positions by telescope id.
+# Checks that the same telescopes have the same position across all files.
+# NOTE: currently hardcoded to only collect MSTS telescope positions (in line with other functions)
+def load_HDF5_auxiliary_data(file_list): 
     telescope_positions = {}
     for filename in file_list:
         with tables.open_file(filename, mode='r') as f:
+            # For every telescope in the telescope table
             for row in f.root.Telescope_Info.iterrows():
-                if row["tel_type"].decode('utf-8') == 'MSTS':
-                    tel_id = row["tel_id"]
-                    position = [row["tel_x"],row["tel_y"],row["tel_z"]]
-                    if tel_id not in telescope_positions:
-                        telescope_positions[tel_id] = position
+                if row['tel_type'].decode('utf-8') == 'MSTS':
+                    if row['tel_id'] not in telescope_positions:
+                        telescope_positions[row['tel_id']] = [row["tel_x"],row["tel_y"],row["tel_z"]]
                     else:
-                        if telescope_positions[tel_id] != position:
-                            raise ValueError("Telescope positions do not match for telescope {} in file {}.".format(tel_id,filename))
+                        if telescope_positions[row['tel_id']] != [row["tel_x"],row["tel_y"],row["tel_z"]]:
+                            raise ValueError("Telescope positions do not match for telescope {} in file {}.".format(row['tel_id'],filename))
     auxiliary_data = {
             'telescope_positions': telescope_positions
             }
@@ -255,49 +252,41 @@ def load_HDF5_metadata(file_list):
 
     return metadata
 
-def load_HDF5_image(data_file,tel_type,metadata,index):
-    telescope_table = data_file.root._f_get_child(tel_type)
-    record = telescope_table[index] 
+def load_HDF5_image(data_file,tel_type,index):
+    record = data_file.root._f_get_child(tel_type)[index]
     telescope_image = []
     
-    image_shape = metadata['image_shapes'][tel_type]
-    
+    # Allocate empty numpy array of shape (len_trace + 1,) to hold trace plus
+    # "empty" pixel at index 0 (used to fill blank areas in image)
     trace = np.empty(shape=(record['image_charge'].shape[0] + 1),dtype=np.float32)
-    # Add 0 as first element in trace (used to fill in blank areas in the image)
+    # Read in the trace from the record 
     trace[0] = 0.0
     trace[1:] = record['image_charge']
     
-    telescope_image = trace[MAPPING_TABLES[tel_type]]
+    # Create image by indexing into the trace using the mapping table, then adding a
+    # dimension to given shape (length,width,1)
+    telescope_image = np.expand_dims(trace[MAPPING_TABLES[tel_type]],2)
   
-    # add dimension to give shape [120,120,1]
-    telescope_image = np.expand_dims(telescope_image,2)
-
     return telescope_image
 
-# Function to get all indices (by HDF5 file) passing a provided cut condition
+# Function to get all indices in each HDF5 file which pass a provided cut condition
+# For single tel mode, returns all MSTS image table indices from events passing the cuts
+# For array-level mode, returns all event table indices from events passing the cuts
 # Cut condition must be a string formatted as a Pytables selection condition
 # (i.e. for table.where()). See Pytables documentation for examples.
 def apply_cuts_HDF5(data_files,cut_condition,model_type):
-    
     indices_by_file = []
     for filename in data_files:
+        # No need to use the multithread-safe file open, as this function is only called once
         with tables.open_file(filename, mode='r') as f:
-            table = f.root.Event_Info
+            # For single tel, get all passing events, then collect all non-zero MSTS image indices into a flat list
             if model_type == 'singletel':
-                indices = []
-                if cut_condition is not None:
-                    image_indices = [row['MSTS_indices'] for row in table.where(cut_condition)]
-                else:
-                    image_indices = [row['MSTS_indices'] for row in table.iterrows()]
-                for indices_vector in image_indices:
-                    for i in indices_vector:
-                        if i != 0:
-                            indices.append(i)
+                passing_events = f.root.Event_Info.where(cut_condition) if cut_condition is not None else f.root.Event_Info.iterrows()
+                indices = [i for row in passing_events for i in row['MSTS_indices'] if i != 0]
+            # For array-level get all passing rows and return a list of all of the indices
             else:
-                if cut_condition is not None:
-                    indices = [row.nrow for row in table.where(cut_condition)]
-                else:
-                    indices = range(table.nrows)
+                indices = [row.nrow for row in table.where(cut_condition)] if cut_condition is not None else range(table.nrows)
+            
             indices_by_file.append(indices)
 
     return indices_by_file
