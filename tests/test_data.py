557f46e12e97e3d439a72da71bb12d3eae53516b
Bryan Kim <bsk2133@columbia.edu>
2018-02-02 20:33:49 -0500

    Added tests for gen_fn_HDF5 and split_indices_lists

---
+++ tests/test_data.py
@@ -0,0 +1,47 @@
+import pytest
+
+from ctalearn.data import gen_fn_HDF5, split_indices_lists
+
+"""
+@pytest.fixture(scope='session')
+def example_HDF5_files(tmpdir_factory):
+   
+
+    h5_file_paths = [file1,file2]
+
+    return h5_file_paths
+
+def test_synchronized_open_file(h5_file_path): 
+
+def test_synchronized_close_file(h5_file_path):
+
+"""
+
+def test_gen_fn_HDF5():
+    
+    file_list = ["test_file1.h5","test_file2.h5"]
+    indices_by_file = [[1,2],[3,4]]
+
+    generator = gen_fn_HDF5(file_list,indices_by_file)
+
+    assert generator.next == (b"test_file1.h5",1)
+    assert generator.next == (b"test_file1.h5",2)
+    assert generator.next == (b"test_file2.h5",3)
+    assert generator.next == (b"test_file2.h5",4)
+
+
+def test_split_indices_lists(indices_lists,validation_split):
+    
+    indices_lists = [[5,9,3,2,1,4,9,2,11,14,15],[1,3,5,7]]
+    validation_split = 0.1
+    
+    training_lists, validation_lists = split_indices_lists(indices_lists,validation_split)
+
+    for i, (train_list, val_list) in enumerate(zip(training_lists, validation_lists)):
+        assert len(train_list) + len(val_list) == len(indices_lists[i])
+        
+        indices_before = set(indices_lists[i])
+        indices_after = set(train_list + val_list)
+
+        assert indices_before == indices_after
+
