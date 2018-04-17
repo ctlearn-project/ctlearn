47465fd046f8a3d8ec368e0d93c02d723139f6aa
Ari Brill <aryeh.brill@gmail.com>
2018-04-17 10:57:54 -0400

    Plot multiple ROC curves on single plot

--- misc/plot_roc_curves.py
+++ misc/plot_roc_curves.py
@@ -1,32 +1,47 @@
+from itertools import cycle
+
 from matplotlib import pyplot as plt
 import numpy as np
-import sklearn
+import sklearn.metrics
 
 import argparse
 
 parser = argparse.ArgumentParser(
     description=("Plot ROC curves."))
-parser.add_argument(
-    'predictions_file',
-    help='csv file of predictions')
-parser.add_argument(
-    '--BDT_predictions_file',
-    default=None)
+parser.add_argument('predictions_list_file',
+        help='list of paths to prediction files with names')
 parser.add_argument(
     "--output_filename",
     help="name for output plot file",
     default="roc_curves.png")
 args = parser.parse_args()
 
-predictions = np.genfromtxt ('file.csv', delimiter=",")
-true_labels = csv[:,1]
-classifier_values = csv[:,4]
+# Predictions list has the format: classifer_name, classifier_path
+classifiers = []
+with open(args.predictions_list_file) as f:
+    for line in f:
+        name, path = line.split(',')
+        classifiers.append([name.strip(), path.strip()])
 
-fpr, tpr, _ = sklearn.metrics.roc_curve(true_labels, classifier_values, pos_label=1)
-auc = sklearn.metrics.auc(fpr,tpr)
-
+# Make the plot
 plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label='CNNRNN (AUC = %0.2f)' % auc)
+colors = cycle(['darkorange', 'aqua', 'cornflowerblue', 'deeppink'])
+
+# Plot the ROC curve for each set of predictions
+for classifier, color in zip(classifiers, colors):
+    classifier_name = classifier[0]
+    predictions_path = classifier[1]
+    predictions = np.genfromtxt(predictions_path, delimiter=',', skip_header=1)
+    true_labels = predictions[:,1]
+    classifier_values = predictions[:,3]
+
+    fpr, tpr, _ = sklearn.metrics.roc_curve(true_labels, classifier_values)
+    auc = sklearn.metrics.auc(fpr, tpr)
+
+    plt.plot(fpr, tpr, color=color, lw=2,
+            label=classifier_name+' (AUC = {:.2f})'.format(auc))
+
+# Finish the plot
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
@@ -37,5 +52,6 @@ plt.title('Receiver Operating Characteristic')
 
 plt.legend(loc='lower right')
 
-plt.savefig(args.output_filename, bbox_inches='tight')
+plt.show()
+#plt.savefig(args.output_filename, bbox_inches='tight')
 
