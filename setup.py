ff185cf4c540b684c3956b63c13a709ab39dd22f
Ari Brill <aryeh.brill@gmail.com>
2018-04-18 11:51:28 -0400

    Update version and requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ctalearn',
-      version='0.1.0',
+      version='0.1.1',
       description='Deep learning models for analysis and classification of image data for CTA (the Cherenkov Telescope Array).',
       url='https://github.com/bryankim96/ctalearn',
       license='MIT',
