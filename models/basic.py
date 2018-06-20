867a26a2bb8a20ee54b3df8da4670a854f0f5884
Ari Brill <aryeh.brill@gmail.com>
2018-06-20 14:29:58 -0400

    Configure basic network head models
    Some typos in the example config have been fixed as well.

--- models/basic.py
+++ models/basic.py
@@ -1,9 +1,5 @@
 import tensorflow as tf
 
-BASIC_FC_HEAD_LAYERS = [1024,512,256,128,64]
-
-BASIC_CONV_HEAD_LAYERS = [(64,3),(128,3),(256,3)]
-
 def basic_conv_block(inputs, training, params=None, reuse=None):
 
     with tf.variable_scope("Basic_conv_block", reuse=reuse):
@@ -24,7 +20,7 @@ def basic_conv_block(inputs, training, params=None, reuse=None):
         if bottleneck:
             bottleneck_filters = int(
                     params.get('basicconvblockbottleneckfilters'))
-        batchnorm = bool(params.get('basicconvblockbatchnorm'))
+        batchnorm = bool(params.get('basicconvblockbatchnorm', False))
         
         x = inputs
         if batchnorm:
@@ -53,37 +49,62 @@ def basic_conv_block(inputs, training, params=None, reuse=None):
 
     return x
 
-def basic_head_fc(inputs, params=None, training=True):
+def basic_fc_head(inputs, training, params=None):
 
-    # Get hyperparameters
+    # Get standard hyperparameters
     if params is None: params = {}
     num_classes = params.get('num_classes', 2)
+    bn_momentum = float(params.get('batchnormdecay', 0.99))
+    
+    # Get custom hyperparameters
+    layers = [int(l) for l in params.get('basicfcheadlayers').split('|')]
+    batchnorm = bool(params.get('basicfcheadbatchnorm', False))
 
     x = tf.layers.flatten(inputs)
 
-    for i, num_units in enumerate(BASIC_FC_HEAD_LAYERS):
-        x = tf.layers.dense(x, units=num_units, activation=tf.nn.relu, name="fc_{}".format(i+1))
-        x = tf.layers.batch_normalization(x, training=training)
+    for i, units in enumerate(layers):
+        x = tf.layers.dense(x, units=units, activation=tf.nn.relu,
+                name="fc_{}".format(i+1))
+        if batchnorm:
+            x = tf.layers.batch_normalization(x, momentum=bn_momentum,
+                    training=training)
 
     logits = tf.layers.dense(x, units=num_classes, name="logits")
 
     return logits
 
-def basic_head_conv(inputs, params=None, training=True):
+def basic_conv_head(inputs, training, params=None):
 
-    # Get hyperparameters
+    # Get standard hyperparameters
     if params is None: params = {}
     num_classes = params.get('num_classes', 2)
+    bn_momentum = float(params.get('batchnormdecay', 0.99))
+    
+    # Get custom hyperparameters
+    filters_list = [int(f) for f in
+            params.get('basicconvheadfilters').split('|')]
+    kernels = [int(k) for k in
+            params.get('basicconvheadkernels').split('|')]
+    avg_pool = bool(params.get('basicconvheadavgpool', True))
+    batchnorm = bool(params.get('basicconvheadbatchnorm', False))
 
     x = inputs
 
-    for i, (filters, kernel_size) in enumerate(BASIC_CONV_HEAD_LAYERS):
-        x = tf.layers.conv2d(x,filters=filters,kernel_size=kernel_size,activation=tf.nn.relu,padding="same",name="conv_{}".format(i+1))
-        x = tf.layers.batch_normalization(x, training=training)
+    for i, (filters, kernel_size) in enumerate(zip(filters_list, kernels)):
+        x = tf.layers.conv2d(x, filters=filters, kernel_size=kernel_size,
+                activation=tf.nn.relu, padding="same",
+                name="conv_{}".format(i+1))
+        if batchnorm:
+            x = tf.layers.batch_normalization(x, momentum=bn_momentum,
+                    training=training)
 
-    pool = tf.layers.average_pooling2d(x, pool_size=x.get_shape().as_list()[1], strides=1, name="global_avg_pool")
-    flatten = tf.layers.flatten(pool)
-
+    # Average over remaining width and length
+    if avg_pool:
+        x = tf.layers.average_pooling2d(x,
+                pool_size=x.get_shape().as_list()[1],
+                strides=1, name="global_avg_pool")
+    
+    flatten = tf.layers.flatten(x)
     logits = tf.layers.dense(flatten, units=num_classes, name="logits")
 
     return logits
