aa29ed77d0bc2d4f0670620c500c8099d3282d55
Bryan Kim <bryan.sanghyuk.kim@gmail.com>
2018-07-03 06:17:06 -0400

    Updated setting/param names to match new config files.

--- models/single_tel.py
+++ models/single_tel.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def single_tel_model(features, params, training):
     
     # Reshape inputs into proper dimensions
-    num_telescope_types = len(params['processed_telescope_types']) 
+    num_telescope_types = len(params['selected_telescope_types']) 
     if num_telescope_types != 1:
         raise ValueError('Must use a single telescope type for single telescope model. Number used: {}'.format(num_telescope_types))
     telescope_type = params['processed_telescope_types'][0]
@@ -17,15 +17,15 @@ def single_tel_model(features, params, training):
     telescope_data = tf.reshape(telescope_data,[-1,image_width,image_length,image_depth], name="telescope_images")
 
     # Load neural network model
-    sys.path.append(params['modeldirectory'])
-    network_module = importlib.import_module(params['networkmodule'])
-    network = getattr(network_module, params['networkfunction'])
+    sys.path.append(params['model_directory'])
+    network_module = importlib.import_module(params['NetworkModule'])
+    network = getattr(network_module, params['NetworkFunction'])
 
     with tf.variable_scope("Network"):
         output = network(telescope_data, params=params, training=training)
 
-    if params['pretrainedweights']:
-        tf.contrib.framework.init_from_checkpoint(params['pretrainedweights'],{'Network/':'Network/'})
+    if params['PretrainedWeights']:
+        tf.contrib.framework.init_from_checkpoint(params['PretrainedWeights'],{'Network/':'Network/'})
 
     output_flattened = tf.layers.flatten(output)
 
