ffcfda8f2761ee512c9d6e18b490eacd7763b57e
Ari Brill <aryeh.brill@gmail.com>
2018-07-09 14:00:54 -0400

    Include pixel position files in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 setup(name='ctalearn',
       version='0.2.0',
-      description='Deep learning for analysis and classification of image data for Imaging Astmospheric Cherenkov Telescopes, especially the Cherenkov Telescope Array (CTA)',
+      description='Deep learning for analysis and classification of image data for Imaging Atmospheric Cherenkov Telescopes, especially the Cherenkov Telescope Array (CTA)',
       url='https://github.com/bryankim96/ctalearn',
       license='MIT',
       packages=['ctalearn'],
+      include_package_data=True,
       dependencies=[],
       dependency_links=[],
       zip_safe=False)
