c4db06d917b1434037e397f5bc65814c05c229e4
Ari Brill <aryeh.brill@gmail.com>
2018-07-23 10:30:45 -0400

    Upgrade train_configurations script
    The script train_configurations.py has been extensively rewritten with
    the following major changes:
    - Add ability to group together config settings when running over
    configurations. This means the script can be used to automate training
    over a set of specified configurations, not just for a generic
    hyperparameter search.
    - Combine the changing_configs file into the base configuration file by
    using two special Multiple Configurations sections. These sections are
    ignored when simply running a model. This setup is simpler since the
    user needs to track fewer files, keeps the settings for a set of
    multiple runs all in one place, and takes fuller advantage of YAML.
    - Simplify run combinations dictionary file. Instead of storing the full
    key for each config option, just save a YAML file mapping a user-provided
    config nicknames to values. This is better because the purpose of this
    output dictionary is just to give a handy human and machine readable
    reference for which run corresponded to which settings without having to
    examine the config file in each directory separately. The full config
    files for reproducibility are already being stored separately by
    run_model.py.

--- scripts/train_configurations.py
+++ scripts/train_configurations.py
@@ -30,12 +30,10 @@ of runs.
 """
 
 import argparse
-import itertools
 import os
 import sys
 
 import numpy as np
-
 import yaml
 
 from ctlearn.run_model import run_model
@@ -44,22 +42,12 @@ from ctlearn.run_model import run_model
 parser = argparse.ArgumentParser(
     description=("Train a ctalearn model over different hyperparameter choices."))
 parser.add_argument(
-        'constant_config_path',
-        help="path to configuration file with constant options")
+        'config_path',
+        help="path to configuration file containing base options and Multiple Configurations sections")
 parser.add_argument(
-        'changing_configs_path',
-        help="path to file listing configuration options to change")
-parser.add_argument(
-        'run_combinations_path',
-        help="path to file in which to save combinations for each run")
-parser.add_argument(
-        'model_dir',
-        help="model directory path for checkpoints and logging"
-        )
-parser.add_argument(
-        'combinations_multiplier',
-        type=int,
-        help="number of times to run over each discrete combination")
+        '--mode',
+        default="train",
+        help="Mode to run in (train/predict)")
 parser.add_argument(
         '--debug',
         action='store_true',
@@ -71,106 +59,130 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-def yield_config(constant_config_path, changing_configs, model_dir,
-        combinations_multiplier):
-    # List tuples of the config coordinates and value for each value of each
-    # discrete config
-    discrete_configs = [[(c['keys'], v) for v in c['values']]
-            for c in changing_configs if not c['random']]
-    # Get all the combinations
-    discrete_combinations = (list(itertools.product(*discrete_configs)) *
-            combinations_multiplier)
-    combinations = [d + tuple([(c['keys'], c['value_fn']()) for
-        c in changing_configs if c['random']]) for d in discrete_combinations]
+def parse_range_values(range_values, num_grouped_range_values):
+
+    def log_space(lower_bound, upper_bound, num_values):
+        return np.logspace(np.log10(lower_bound), np.log10(upper_bound),
+                num_values)
+
+    def random_log_uniform(lower_bound, upper_bound, num_values):
+        return np.power(10.0, np.random.uniform(np.log10(lower_bound),
+            np.log10(upper_bound), num_values))
+
+    settings_to_value_fn = {
+            ('linear', 'grid'): np.linspace,
+            ('log', 'grid'): log_space,
+            ('linear', 'random'): np.random.uniform,
+            ('log', 'random'): random_log_uniform
+            }
+
+    value_fn = settings_to_value_fn[(range_values['spacing'],
+        range_values['selection'])]
+    if range_values['grouped']:
+        values = value_fn(
+                range_values['lower_bound'],
+                range_values['upper_bound'],
+                num_grouped_range_values)
+        values = {'__range_group_'+str(i): val for i, val in enumerate(values)}
+        value_type = 'range_grouped'
+    else:
+        values = value_fn(
+                range_values['lower_bound'],
+                range_values['upper_bound'],
+                range_values['num_values'])
+        values = list(values)
+        value_type = 'range_ungrouped'
+    return values, value_type
+
+# Return a list of all the combinations resulting from adding each of the
+# values to each of the current combinations except for those belonging to
+# incompatible groups
+def add_values_to_combinations(config_name, values, value_type, combinations):
+    if value_type in ['list', 'range_ungrouped']:
+        groups_by_value = {val: (None, set()) for val in values}
+    elif value_type in ['grouped', 'range_grouped']:
+        groups_by_value = {val: (group, set([g for g in values.keys() if not
+            group == g])) for group, val in values.items()}
+    new_combinations = []
+    for value, (group, excluded_groups) in groups_by_value.items():
+        for combination in combinations:
+            if not group or group not in combination['excluded_groups']:
+                new_combination = {k: v for k, v in combination.items()}
+                new_combination['excluded_groups'] |= excluded_groups
+                new_combination['config_values'][config_name] = value
+                new_combinations.append(new_combination)
+    return new_combinations
+
+def make_config_from_combination(combination, config_name_to_keys):
+    changed_config = {}
+    for config_name, value in combination['config_values'].items():
+        config_keys = config_name_to_keys['config_name']
+        section = changed_config
+        for key in keys[:-1]:
+            section[key] = {}
+            section = section[key]
+        section[keys[-1]] = value
+    return changed_config
+
+def make_configurations(base_config, changing_configurations, settings):
     
-    for run_num, combination in enumerate(combinations):
-        with open(args.constant_config_path, 'r') as constant_config_file:
-            config = yaml.load(constant_config_file)
-        combination_dict = {}
-        # Add the changing config options to the constant config
-        for (keys, value) in combination:
-            section = config
-            combination_dict_section = combination_dict
-            for i in range(len(keys) - 1):
-                if keys[i] not in section:
-                    section[keys[i]] = {}
-                section = section[keys[i]]
+    # List of all the combinations of changing config options
+    # Start with the trivial combination of no options included
+    changing_config_combinations = [{
+        'excluded_groups': set(),
+        'config_values' = {} # items are config_name: value
+        }]
+    # Also store the list of config keys corresponding to each config name
+    config_name_to_keys = {}
 
-                if keys[i] not in combination_dict_section:
-                    combination_dict_section[keys[i]] = {}
-                combination_dict_section = combination_dict_section[keys[i]]
+    # List the combinations
+    for config_name, config_settings in changing_configurations:
+        if config_settings['value_type'] == 'range':
+            values, value_type = parse_range_values(config_settings['values'],
+                    settings['num_grouped_range_values'])
+        elif config_settings['value_type'] in ['list', 'grouped']:
+            values = config_settings['values']
+            value_type = config_settings['value_type']
+        changing_config_combinations = add_values_to_combinations(config_name,
+            values, value_type, changing_config_combinations)
+        config_name_to_keys[config_name] = config_settings['config']
 
-            section[keys[-1]] = value
-            combination_dict_section[keys[-1]] = value
+    # Construct a dictionary of combinations and list of configurations
+    combinations = {}
+    configurations = []
+    base_model_dir = base_config['Logging']['model_directory']
+    for run_num, combination in enumerate(changing_config_combinations):
+        run_name = 'run' + str(run_num)
+        combinations[run_name] = combination['config_values']
+        changed_config = make_config_from_combination(combination,
+                config_name_to_path)
         # Set the model directory to that corresponding to this run
-        run_model_directory = os.path.join(model_dir, 'run'+str(run_num))
-        config['Logging']['model_directory'] = run_model_directory
-        yield config, combination_dict
+        run_model_dir = os.path.join(base_model_dir, run_name)
+        changed_config['Logging']['model_directory'] = run_model_dir
+        configurations.append((run_name, {**base_config, **changed_config}))
+        
+    return combinations, configurations
 
-def get_changing_configs(section, keys, changing_configs):
-    for k, v in section.items(): 
-        if isinstance(v, dict):
-            keys.append(k)
-            get_changing_configs(v, keys, changing_configs)
-        else:
-            config = {}
-            config['keys'] = keys + [k]
-            iteration_type, settings = v 
-            if iteration_type == 'discrete':  
-                config['random'] = False
-                config['values'] = settings
-            elif iteration_type == 'grid_range':
-                config['random'] = False
-                lower_bound, upper_bound, spacing, num = settings
-                lower_bound = float(lower_bound)
-                upper_bound = float(upper_bound)
-                num = int(num)
-                if spacing == 'linear':
-                    config['values'] = np.linspace(lower_bound, upper_bound, num)
-                elif spacing == 'log':
-                    config['values'] = np.logspace(np.log10(lower_bound),
-                            np.log10(upper_bound), num)
-            elif iteration_type == 'random_range':
-                config['random'] = True
-                lower_bound, upper_bound, spacing = settings
-                lower_bound = float(lower_bound)
-                upper_bound = float(upper_bound)
-                if spacing == 'linear':
-                    config['value_fn'] = lambda: np.random.uniform(lower_bound,
-                            upper_bound)
-                elif spacing == 'log':
-                    config['value_fn'] = lambda: np.power(10.0,
-                            np.random.uniform(
-                                np.log10(lower_bound), np.log10(upper_bound)))
+# Load config file containing all unchanging options as well as 
+# Multiple Configurations sections
+with open(args.config_path, 'r') as config_file:
+    raw_config = yaml.load(config_file)
+base_config = {key:val for key, val in raw_config.items() if key not in 
+        ["Multiple Configurations Settings", "Multiple Configurations Values"]}
+settings = raw_config["Multiple Configurations Settings"]
+changing_configurations = raw_config["Multiple Configurations Values"]
 
-            changing_configs.append(config)
+# Generate a configuration for each combination of settings as specified
+combinations, configurations = make_configurations(base_config,
+        changing_configurations, settings)
 
-# Load constant config
-with open(args.constant_config_path, 'r') as constant_config_file:
-    constant_config = yaml.load(constant_config_file)
+# Save a dictionary storing the configuration combination set for each run
+# to a file for convenient lookup
+with open(settings['run_combinations_path'], 'w') as combinations_file:
+    yaml.dump(combinations, combinations_file, default_flow_style=False)
 
-# Load a file specifying config options to run over
-changing_configs = []
-keys = []
-with open(args.changing_configs_path, 'r') as changing_config_file:
-    changing_options_config = yaml.load(changing_config_file)
-    get_changing_configs(changing_options_config, keys, changing_configs)
-    print(changing_configs)
-
-# write constant part of config
-with open(args.run_combinations_path, 'a') as runs_file:
-    runs_file.write("Constant settings \n")        
-    yaml.dump(constant_config, runs_file, default_flow_style=False)
-    runs_file.write("\n")
-
-for run_num, (config, combination_dict) in enumerate(yield_config(
-    args.constant_config_path, changing_configs, args.model_dir,
-    args.combinations_multiplier)):
-    print("Training run {}...".format(run_num))
-    # Save the hyperparameter combination for each run to a file
-    with open(args.run_combinations_path, 'a') as runs_file:
-        runs_file.write("Run {}\n".format(run_num))        
-        yaml.dump(combination_dict, runs_file, default_flow_style=False)
-        runs_file.write("\n")
-    run_model(config, mode='train', debug=args.debug,
+# Run a model for each configuration combination
+for run_name, config in configurations:
+    print("Running", run_name+"...")
+    run_model(config, mode=args.mode, debug=args.debug,
             log_to_file=args.log_to_file)
