9d18fefa256e539928a51477d3f3979281a15342
Ari Brill <aryeh.brill@gmail.com>
2018-07-26 16:01:45 -0400

    Fix errors in bounding box script

--- scripts/visualize_bounding_boxes.py
+++ scripts/visualize_bounding_boxes.py
@@ -1,9 +1,9 @@
-import random
-import os
 import argparse
+import itertools
+import os
 
-import numpy as np
 import cv2
+import numpy as np
 from PIL import Image, ImageDraw
 
 from ctlearn.data_loading import HDF5DataLoader
@@ -19,6 +19,7 @@ if __name__ == "__main__":
         help='List of HDF5 (pytables) files to examine images from')
     parser.add_argument(
         "--num_images",
+        type=int,
         help="number of randomly sampled images to examine",
         default=10)
     parser.add_argument(
@@ -27,16 +28,23 @@ if __name__ == "__main__":
         default='MSTS')
     parser.add_argument(
         "--bounding_box_size",
+        type=int,
         help="Side length of square bounding box",
         default=48)
     parser.add_argument(
         "--picture_threshold",
+        type=float,
         help="First threshold (applied image-wide)",
         default=5.5)
     parser.add_argument(
         "--boundary_threshold",
+        type=float,
         help="Second threshold (applied on pixels surviving the first cut)",
         default=1.0)
+    parser.add_argument(
+        "--normalization",
+        help="Type of normalization to apply (default: None)",
+        default=None)
     parser.add_argument(
         "--save_original",
         help="whether to save the original (un-cleaned) image",
@@ -47,8 +55,12 @@ if __name__ == "__main__":
         action="store_true")
     parser.add_argument(
         "--save_cropped",
-        help="wether to save the cropped image",
+        help="whether to save the cropped image",
         action="store_true")
+    parser.add_argument(
+        "--base_filename",
+        help="base filename for saving files",
+        default='image')
     parser.add_argument(
         "--save_dir",
         help="directory to save the image files",
@@ -66,7 +78,7 @@ if __name__ == "__main__":
                 file_list.append(line)
 
     data_loader = HDF5DataLoader(file_list, 
-            example_type="singletel", 
+            example_type="single_tel", 
             selected_tel_type=args.tel_type,
             mode="test")
 
@@ -81,49 +93,45 @@ if __name__ == "__main__":
 
     data_processor = DataProcessor(**data_processing_settings) 
 
-    count = 0
-    while count < args.num_images:
-        for run_number, event_number, tel_id in data_loader.examples:
-        
-            example = data_loader.get_example(run_number, event_number, tel_id)
+    example_generator, _ = data_loader.get_example_generators()
+    for i, example_identifiers in enumerate(itertools.islice(example_generator(), args.num_images)):
 
-            # get only the first channel (charge) of an image of arbitrary depth
-            image_charge = example[0][:,:,0]
+        example = data_loader.get_example(*example_identifiers)
 
-            if args.save_original:
-                im = Image.fromarray(((image_charge - np.min(image_charge)) * 255 / np.max(image_charge)).astype('uint8'))
-                im.save(os.path.join(save_dir,base_filename + '_{}_original.jpg'.format(i)))
+        image = example[0]
+        # get only the first channel (charge) of an image of arbitrary depth
+        image_charge = image[:,:,0]
 
-            if args.save_cleaned:
-                # get only the first channel (charge) of an image of arbitrary depth
-                image_charge = example[0][:,:,0]
+        if args.save_original:
+            im = Image.fromarray(((image_charge - np.min(image_charge)) * 255 / np.max(image_charge)).astype('uint8'))
+            im.save(os.path.join(save_dir,args.base_filename + '_{}_original.jpg'.format(i)))
 
-                data_processor.return_cleaned_images = True
-                data_processor.bounding_box_sizes[args.tel_type] = ImageMapper.image_shapes[args.tel_type][0]
+        if args.save_cleaned:
+            data_processor.return_cleaned_images = True
+            data_processor.bounding_box_sizes[args.tel_type] = ImageMapper.image_shapes[args.tel_type][0]
 
-                cleaned_image, _, _ = data_processor._crop_image(image_charge, args.tel_type) 
+            cleaned_image, _, _ = data_processor._crop_image(image, args.tel_type) 
+            cleaned_image = cleaned_image[:,:,0]
 
-                cleaned_image = Image.fromarray(((cleaned_image - np.min(cleaned_image)) * 255 / np.max(cleaned_image)).astype('uint8'))
-                cleaned_image.save(os.path.join(save_dir,base_filename + '_{}_cleaned.jpg'.format(i)))
+            cleaned_image = Image.fromarray(((cleaned_image - np.min(cleaned_image)) * 255 / np.max(cleaned_image)).astype('uint8'))
+            cleaned_image.save(os.path.join(save_dir,args.base_filename + '_{}_cleaned.jpg'.format(i)))
 
-                data_processor.return_cleaned_images = False
-                data_processor.bounding_box_sizes[args.tel_type] = args.bounding_box_size
+            data_processor.return_cleaned_images = False
+            data_processor.bounding_box_sizes[args.tel_type] = args.bounding_box_size
 
-            image_cropped, x_0, y_0 = data_processor._crop_image(image_charge, args.tel_type)
+        image_cropped, x_0, y_0 = data_processor._crop_image(image, args.tel_type)
 
-            if args.save_cropped:
-                image_cropped = image_cropped[:,:,0]
-                im_cropped = Image.fromarray(((image_cropped - np.min(image_cropped)) * 255 / np.max(image_cropped)).astype('uint8'))
-                im_cropped.save(os.path.join(save_dir,base_filename + '_{}_cropped.jpg'.format(i)))
+        if args.save_cropped:
+            image_cropped = image_cropped[:,:,0]
+            im_cropped = Image.fromarray(((image_cropped - np.min(image_cropped)) * 255 / np.max(image_cropped)).astype('uint8'))
+            im_cropped.save(os.path.join(save_dir,args.base_filename + '_{}_cropped.jpg'.format(i)))
 
-            x_min = int(round(x_0 - args.bounding_box_size/2))
-            x_max = int(round(x_0 + args.bounding_box_size/2)) - 1
-            y_min = int(round(y_0 - args.bounding_box_size/2))
-            y_max = int(round(y_0 + args.bounding_box_size/2)) - 1
+        x_min = int(round(x_0 - args.bounding_box_size/2))
+        x_max = int(round(x_0 + args.bounding_box_size/2)) - 1
+        y_min = int(round(y_0 - args.bounding_box_size/2))
+        y_max = int(round(y_0 + args.bounding_box_size/2)) - 1
 
-            im_box = Image.fromarray(((image_charge - np.min(image_charge)) * 255 / np.max(image_charge)).astype('uint8'))
-            draw = ImageDraw.Draw(im_box)
-            draw.rectangle([y_min, x_min, y_max, x_max],outline='yellow')
-            im_box.save(os.path.join(save_dir,base_filename + '_{}_box.jpg'.format(i)))
-
-
+        im_box = Image.fromarray(((image_charge - np.min(image_charge)) * 255 / np.max(image_charge)).astype('uint8'))
+        draw = ImageDraw.Draw(im_box)
+        draw.rectangle([y_min, x_min, y_max, x_max],outline='yellow')
+        im_box.save(os.path.join(save_dir,args.base_filename + '_{}_box.jpg'.format(i)))
