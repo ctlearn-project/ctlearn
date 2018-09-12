8dcb96f8807c5d72060cf96f66a66745d065541a
Jsevillamol <jsevillamol@outlook.com>
2018-09-12 20:30:39 +0200

    Add script to summarize results from multiple runs

---
+++ scripts/summarize_results.py
@@ -0,0 +1,53 @@
+"""
+Run this script from the folder where the run folders are situated.
+It will automatically read the results and create a table summarizing them.
+It currently has columns for telescope type, array or single tel, AUROC
+"""
+
+import os
+import yaml
+import re
+import pandas as pd
+
+rows = []
+
+for folder in os.listdir('.'):
+	if not os.path.isdir(folder): continue
+	dir_ = os.listdir(folder)
+
+	# open configuration file
+	r = re.compile('.*config\.yml')
+	matches = list(filter(r.match, dir_))
+	assert(len(matches) == 1)
+	fn = folder + '/' + matches[0]
+
+	with open(fn, 'r') as stream:
+		config = yaml.load(stream)
+
+	# open log
+	r = re.compile('.*logfile\.log')
+	matches = list(filter(r.match, dir_))
+	assert(len(matches) == 1)
+	fn = folder + '/' + matches[0]
+
+	with open(fn, 'r') as stream:
+		r = re.compile('INFO:Saving dict for global step .*')
+		matches = list(filter(r.match, stream))
+		assert(len(matches) > 0)
+		val_info = matches[-1]
+
+	# compile the info we need
+	input_type = config['Data']['Loading']['example_type']
+	tel_type = config['Data']['Loading']['selected_tel_type']
+	
+	num_epochs = config['Training']['num_epochs']
+	num_training_steps_per_val = config['Training']['num_training_steps_per_validation']
+
+	auroc = float(re.findall(r"auc = [-+]?\d*\.*\d+", val_info)[0][6:])
+
+	rows.append({'input_type': input_type, 'tel_type': tel_type, 'auroc': auroc})
+
+df = pd.DataFrame(rows, columns=['input_type', 'tel_type', 'auroc'])
+df = df.sort_values(by=['input_type', 'tel_type'])
+
+df.to_csv('summary.csv')
