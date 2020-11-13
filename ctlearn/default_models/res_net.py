adf87eefdc2048b3f4153b1269edce7c0873617a
TjarkMiener <tjark.miener4692@googlemail.com>
2020-11-13 21:29:47 +0100

    init layer and pooling optional for the ResNets
    We noticed that the model performance is better if the init layer and pooling is skipped. So we made it optinal. Also there was a bug and the init layer and pooling was also skipped before.

--- ctlearn/default_models/res_net.py
+++ ctlearn/default_models/res_net.py
@@ -18,16 +18,19 @@ def res_net_model(features, model_params, example_description, training):
 
     with tf.variable_scope("Network"):
 
+       x = telescope_data
        # The original ResNet implementation use this padding, but we pad the images in the ImageMapper.
        #x = tf.pad(telescope_data, tf.constant([[3, 3], [3, 3]]), name='conv1_pad')
-       init_layer = model_params['res_net']['init_layer']
-       x = tf.layers.conv2d(telescope_data, filters=init_layer['filters'], kernel_size=init_layer['kernel_size'],
+       init_layer = model_params['res_net'].get('init_layer', False)
+       if init_layer:
+           x = tf.layers.conv2d(x, filters=init_layer['filters'], kernel_size=init_layer['kernel_size'],
                     strides=init_layer['strides'], name='conv1_conv')
        #x = tf.pad(x, tf.constant([[1, 1], [1, 1]]), name='pool1_pad')
-       init_max_pool = model_params['res_net']['init_max_pool']
-       x = tf.layers.max_pooling2d(x, init_max_pool['size'], strides=init_max_pool['strides'], name='pool1_pool')
+       init_max_pool = model_params['res_net'].get('init_max_pool', False)
+       if init_max_pool:
+           x = tf.layers.max_pooling2d(x, init_max_pool['size'], strides=init_max_pool['strides'], name='pool1_pool')
 
-       output = network(telescope_data, params=model_params)
+       output = network(x, params=model_params)
        output = tf.reduce_mean(output, axis=[1,2], name='global_avgpool')
 
     if model_params['res_net']['pretrained_weights']:    tf.contrib.framework.init_from_checkpoint(model_params['res_net']['pretrained_weights'],{'Network/':'Network/'})
