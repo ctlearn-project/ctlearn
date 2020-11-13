56a50e263746e8b2da6b7d5639c3e47f819ab406
TjarkMiener <tjark.miener4692@googlemail.com>
2020-11-13 22:57:48 +0100

    added new type of residual blocks, which is called 'basic' in the literature
    Therefore, I added a new entry in the stacked_res_blocks config section to specify the type of the residual block.

--- ctlearn/default_models/basic.py
+++ ctlearn/default_models/basic.py
@@ -109,23 +109,25 @@ def conv_head(inputs, training, params):
 
 def stacked_res_blocks(inputs, params):
     # Get custom hyperparameters
+    residual_block = params['basic']['stacked_res_blocks'].get('residual_block', 'bottleneck')
     filters_list = [layer['filters'] for layer in
             params['basic']['stacked_res_blocks']['architecture']]
     blocks_list = [layer['blocks'] for layer in
             params['basic']['stacked_res_blocks']['architecture']]
     attention = params['basic'].get('attention', None)
 
-    x = stack_fn(inputs, filters_list[0], blocks_list[0], stride=1, attention=attention, name='conv2')
+    x = stack_fn(inputs, filters_list[0], blocks_list[0], residual_block, stride=1, attention=attention, name='conv2')
     for i, (filters, blocks) in enumerate(zip(filters_list[1:], blocks_list[1:])):
-       x = stack_fn(x, filters, blocks, attention=attention, name='conv' + str(i+3))
+       x = stack_fn(x, filters, blocks, residual_block, attention=attention, name='conv' + str(i+3))
     return x
 
-def stack_fn(inputs, filters, blocks, stride=2, attention=None, name=None):
+def stack_fn(inputs, filters, blocks, residual_block, stride=2, attention=None, name=None):
     """A set of stacked residual blocks.
     Arguments:
       inputs: input tensor.
       filters: integer, filters of the bottleneck layer in a block.
       blocks: integer, blocks in the stacked blocks.
+      residual_block: string, type of residual block.
       stride: default 2, stride of the first layer in the first block.
       attention: default None, squeeze excite ratio for the
           attention mechanism.
@@ -133,13 +135,18 @@ def stack_fn(inputs, filters, blocks, stride=2, attention=None, name=None):
     Returns:
       Output tensor for the stacked blocks.
     """
-    x = res_block(inputs, filters, stride=stride, attention=attention, name=name + '_block1')
+
+    res_blocks = {
+        'basic': basic_residual_block,
+        'bottleneck': bottleneck_residual_block
+    }
+    x = res_blocks[residual_block](inputs, filters, stride=stride, attention=attention, name=name + '_block1')
     for i in range(2, blocks + 1):
-        x = res_block(x, filters, conv_shortcut=False, attention=attention, name=name + '_block' + str(i))
+        x = res_blocks[residual_block](x, filters, conv_shortcut=False, attention=attention, name=name + '_block' + str(i))
     return x
 
-def res_block(inputs, filters, kernel_size=3, stride=1, conv_shortcut=True, attention=None, name=None):
-    """A residual block.
+def basic_residual_block(inputs, filters, kernel_size=3, stride=1, conv_shortcut=True, attention=None, name=None):
+    """A basic residual block.
     Arguments:
       inputs: input tensor.
       filters: integer, filters of the bottleneck layer.
@@ -155,6 +162,49 @@ def res_block(inputs, filters, kernel_size=3, stride=1, conv_shortcut=True, atte
     """
 
     with tf.variable_scope("Basic_res_block"):
+
+        if conv_shortcut:
+            shortcut = tf.layers.conv2d(inputs, filters=filters, kernel_size=1,
+                           strides=stride, name=name + '_0_conv')
+        else:
+            shortcut = inputs
+
+        x = tf.layers.conv2d(inputs, filters=filters, kernel_size=kernel_size,
+                strides=stride, padding='same', activation=tf.nn.relu, name=name + '_1_conv')
+        x = tf.layers.conv2d(x, filters=filters, kernel_size=kernel_size,
+                padding='same', activation=tf.nn.relu, name=name + '_2_conv')
+
+        # Attention mechanism
+        if attention is not None:
+            if attention['mechanism'] == 'Squeeze-and-Excitation':
+                x = squeeze_excite_block(x, attention['ratio'], name=name + '_se')
+            elif attention['mechanism'] == 'Channel-Squeeze-and-Excitation':
+                x = channel_squeeze_excite_block(x, attention['ratio'], name=name + '_cse')
+            elif attention['mechanism'] == 'Spatial-Squeeze-and-Excitation':
+                x = spatial_squeeze_excite_block(x, name=name + '_sse')
+
+        x = tf.math.add_n([shortcut, x], name=name + '_add')
+        x = tf.nn.relu(x, name=name + '_out')
+        return x
+
+def bottleneck_residual_block(inputs, filters, kernel_size=3, stride=1, conv_shortcut=True, attention=None, name=None):
+    """A bottleneck residual block.
+    Arguments:
+      inputs: input tensor.
+      filters: integer, filters of the bottleneck layer.
+      kernel_size: default 3, kernel size of the bottleneck layer.
+      stride: default 1, stride of the first layer.
+      conv_shortcut: default True, use convolution shortcut if True,
+          otherwise identity shortcut.
+      squeeze_excite_ratio: default 0, squeeze excite ratio for the
+          attention mechanism.
+      name: string, block label.
+    Returns:
+      Output tensor for the residual block.
+    """
+
+    with tf.variable_scope("Bottleneck_res_block"):
+
         if conv_shortcut:
             shortcut = tf.layers.conv2d(inputs, filters=4*filters, kernel_size=1,
                            strides=stride, name=name + '_0_conv')
