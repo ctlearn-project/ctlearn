265aeb80b1b5a2982ce8623e7ae17710a95f68bf
Tjark Miener <tjark.miener4692@googlemail.com>
2022-02-11 16:19:52 +0100

    update env files

--- scripts/run_ctlearn_on_Wilkes3.sh
+++ scripts/run_ctlearn_on_Wilkes3.sh
@@ -19,5 +19,5 @@ source /home/ir-mien1/.bashrc
 source /home/ir-mien1/anaconda3/bin/activate ctlearn_tf2
 
 config_file=/home/ir-mien1/tf2/ctlearn/config/v_0_6_0_benchmarks/LSTCam_TRN_lowcut.yml
-/home/ir-mien1/anaconda3/envs/ctlearn_tf2/bin/python3 /home/ir-mien1/anaconda3/envs/ctlearn_tf2/bin/ctlearn $config_file -l /home/ir-mien1/rds/rds-iris-ip007/ir-mien1/logs/LSTCam_multigpus/ -t particletype --log_to_file --mode train -s $SLURM_ARRAY_TASK_ID
+/home/ir-mien1/anaconda3/envs/ctlearn_tf2/bin/python3 /home/ir-mien1/anaconda3/envs/ctlearn_tf2/bin/ctlearn $config_file -l /home/ir-mien1/rds/rds-iris-ip007/ir-mien1/logs/LSTCam_multigpus/ -r particletype --log_to_file --mode train -s $SLURM_ARRAY_TASK_ID
 
