b26ec36b305ebe628afcde1aa94248a8e0f6126e
Daniel Nieto <d.nieto@ucm.es>
2022-03-29 12:56:47 +0200

    Testing RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-autodoc_mock_imports = ["ctlearn"]
 
 # -- Project information -----------------------------------------------------
 
