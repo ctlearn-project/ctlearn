1b0e4ab4122ee82ac175f4b28ea4213f42033278
TjarkMiener <tjark.miener4692@googlemail.com>
2022-03-31 12:40:57 +0200

    fix env in sbatch script

--- scripts/run_ctlearn_on_Wilkes3.sh
+++ scripts/run_ctlearn_on_Wilkes3.sh
@@ -16,6 +16,6 @@ module load python/3.8 cuda/11.2 cudnn/8.1_cuda-11.2
 module load miniconda/3
 
 source /home/ir-mien1/.bashrc
-source /home/ir-mien1/anaconda3/bin/activate ctlearn_tf2
+source /home/ir-mien1/anaconda3/bin/activate ctlearn
 
 /home/ir-mien1/anaconda3/envs/ctlearn/bin/python3 /home/ir-mien1/anaconda3/envs/ctlearn/bin/ctlearn -d TRN -i /home/ir-mien1/rds/rds-iris-ip007/ir-niet1/datasets/DL1_Prod5_merged/gamma-diffuse/train/ -o /home/ir-mien1/rds/rds-iris-ip007/ir-mien1/logs/LSTCam_TRN_energy_ctlearn6/ -t LST_LST_LSTCam -r energy -z 50 -l 0.2 -e 5 -b 64 --log_to_file --mode train -s $SLURM_ARRAY_TASK_ID
