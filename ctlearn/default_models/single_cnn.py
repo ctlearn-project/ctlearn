cfceeaf25150c1d7e94b39b6980eb906131fad21
Sahil Yadav <53268444+sahilyadav27@users.noreply.github.com>
2022-12-06 22:34:48 +0530

    Update single_cnn.py

--- ctlearn/default_models/single_cnn.py
+++ ctlearn/default_models/single_cnn.py
@@ -8,7 +8,10 @@ def single_cnn_model(data, model_params):
 
     # Load neural network model
     network_input_img = tf.keras.Input(shape=data.img_shape, name=f"images")
-    network_input_param = tf.keras.Input(shape=data.prm_shape, name=f"parameters")
+    flag_prm = 0
+    if data.prm_shape != None:
+        flag_prm = 1
+        network_input_param = tf.keras.Input(shape=data.prm_shape, name=f"parameters")
     backbone_name = model_params.get("name", "CNN") + "_block"
     trainable_backbone = model_params.get("trainable_backbone", True)
     pretrained_weights = model_params.get("pretrained_weights", None)
@@ -21,10 +24,12 @@ def single_cnn_model(data, model_params):
     else:
         sys.path.append(model_params["model_directory"])
         engine_cnn_module = importlib.import_module(model_params["engine_cnn"]["module"])
-        engine_mlp_module = importlib.import_module(model_params["engine_mlp"]["module"])
         engine_cnn = getattr(engine_cnn_module, model_params["engine_cnn"]["function"])
-        engine_mlp = getattr(engine_mlp_module, model_params["engine_mlp"]["function"])
-
+        if flag_prm == 1:
+            engine_mlp_module = importlib.import_module(model_params["engine_mlp"]["module"])
+            engine_mlp = getattr(engine_mlp_module, model_params["engine_mlp"]["function"])
+            engine_output_mlp = engine_mlp(network_input_param, params=model_params, name=backbone_name)
+            output_mlp = tf.keras.layers.Flatten()(engine_output_mlp)
         # The original ResNet implementation use this padding, but we pad the images in the ImageMapper.
         # x = tf.pad(telescope_data, tf.constant([[3, 3], [3, 3]]), name='conv1_pad')
         init_layer = model_params.get("init_layer", False)
@@ -45,15 +50,13 @@ def single_cnn_model(data, model_params):
             )(network_input_img)
 
         engine_output_cnn = engine_cnn(network_input_img, params=model_params, name=backbone_name)
-        engine_output_mlp = engine_mlp(network_input_param, params=model_params, name=backbone_name)
-
         output_cnn = tf.keras.layers.GlobalAveragePooling2D(
             name=backbone_name + "_global_avgpool"
         )(engine_output_cnn)
-        output_mlp = tf.keras.layers.Flatten()(engine_output_mlp)
-        concat = tf.keras.layers.Concatenate()([output_cnn, output_mlp])
-        
-        singlecnn_model = tf.keras.Model(inputs=[network_input_img, network_input_param], outputs = [concat], name=backbone_name)
-        
-
-    return singlecnn_model, [network_input_img, network_input_param]
+        if flag_prm == 1:
+            concat = tf.keras.layers.Concatenate()([output_cnn, output_mlp])
+            singlecnn_model = tf.keras.Model(inputs=[network_input_img, network_input_param], outputs = [concat], name=backbone_name)
+            return singlecnn_model, [network_input_img, network_input_param]
+        else:
+            singlecnn_model = tf.keras.Model(network_input_img, output_cnn, name=backbone_name)
+            return singlecnn_model, [network_input_img]
