7fc689ec3025edabce568bbf413b92b03630a94f
rcervinoucm <rcervino@ucm.es>
2024-05-24 12:37:41 +0200

    Update version.py

--- ctlearn/version.py
+++ ctlearn/version.py
@@ -85,7 +85,7 @@ def get_git_describe_version(abbrev=7):
         with open(devnull, "w") as fnull:
             arguments = [GIT_COMMAND, "describe", "--tags", "--abbrev=%d" % abbrev]
             print(CURRENT_DIRECTORY)
-            print(check_output(arguments, cwd=CURRENT_DIRECTORY, stderr=fnull).decode("ascii"))
+            print(check_output(arguments, cwd=CURRENT_DIRECTORY, stderr=fnull).decode("ascii").strip())
             return (
                 check_output(arguments, cwd=CURRENT_DIRECTORY, stderr=fnull)
                 .decode("ascii")
