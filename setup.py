40410ecb2fc597b722291982565d823bae5f7783
rcervinoucm <rcervino@ucm.es>
2024-06-11 14:46:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import setup, find_packages
 from os import path
 
 def getVersionFromFile():
-    print('entro')
     file = open("versionBackup.txt").readlines()
-    print(file)
     for line in file:
         for word in line.split():
-            print('hola')
-            print(word)
             return word
 
 here = path.abspath(path.dirname(__file__))
