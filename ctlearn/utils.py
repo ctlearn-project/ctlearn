cecc3dd81de320f4150a717e1eca580a20cc37e6
Cristian Pozo González <cpozo@iaa.es>
2025-05-28 13:31:15 +0000

    Add the Subarray Descriptions and refactoring the predict task using keras

--- ctlearn/utils.py
+++ ctlearn/utils.py
@@ -2,30 +2,8 @@ from importlib.resources import files, as_file
 
 from ctapipe.core import Provenance
 from ctapipe.core.traits import TraitError
-from ctapipe.instrument import SubarrayDescription
-from ctapipe.instrument.optics import FocalLengthKind
 
-
-__all__ = ["get_lst1_subarray_description", "validate_trait_dict"]
-
-def get_lst1_subarray_description(focal_length_choice=FocalLengthKind.EFFECTIVE):
-    """
-    Load subarray description from bundled file
-    
-    Parameters
-    ----------
-    focal_length_choice : FocalLengthKind
-        Choice of focal length to use.  Options are ``FocalLengthKind.EQUIVALENT``
-        and ``FocalLengthKind.EFFECTIVE``. Default is ``FocalLengthKind.EFFECTIVE``.
-
-    Returns
-    -------
-    SubarrayDescription
-        Subarray description of the LST-1 telescope.
-    """
-    with as_file(files("ctlearn") / "resources/LST-1_SubarrayDescription.h5") as path:
-        Provenance().add_input_file(path, role="SubarrayDescription")
-        return SubarrayDescription.from_hdf(path, focal_length_choice=focal_length_choice)
+__all__ = ["validate_trait_dict"]
 
 def validate_trait_dict(dict, required_keys):
     """
