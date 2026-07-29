42100499ca53d0cf155e4440bd38f89787197ec3
Tjark Miener <tjark.miener4692@googlemail.com>
2026-07-29 09:16:35 +0200

    added prediction tools in init

--- ctlearn/tools/__init__.py
+++ ctlearn/tools/__init__.py
@@ -1,2 +1,11 @@
 """ctlearn command line tools.
 """
+
+from ctlearn.tools.predict_LST1 import LST1PredictionTool
+from ctlearn.tools.predict_model import MonoPredictCTLearnModel, StereoPredictCTLearnModel
+
+__all__ = [
+    "LST1PredictionTool",
+    "MonoPredictCTLearnModel",
+    "StereoPredictCTLearnModel",
+]
