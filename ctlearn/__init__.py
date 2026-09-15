b3f50ef78e991593be54608aa9f410325f48f868
Cristian Pozo González <cpozo@iaa.es>
2026-09-15 12:20:47 +0000

    Add the Keras 2 models support

--- ctlearn/__init__.py
+++ ctlearn/__init__.py
@@ -1,3 +1,12 @@
+import os
+import sys
+if os.environ.get("TF_USE_LEGACY_KERAS") == "1":
+    try:
+        import tf_keras
+        sys.modules["keras"] = tf_keras
+    except ImportError:
+        pass
+
 from ._version import __version__
 
 __all__ = ["__version__"]
